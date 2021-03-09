40d017f72752a114206dcad1c7b750a4db40fd82
PerishCode <perishcode@gmail.com>
2021-03-09 10:19:02 +0800

    local development configuration

--- src/testcase/react/index.tsx
+++ src/testcase/react/index.tsx
@@ -9,35 +9,35 @@ import transformer from '@modules/react/core/transformer/sync'
 
 // //验证基本功能
 // export default function TestCase() {
-//   return (
-//     <XForm
-//       schema={{
-//         [__render__]: [XObject],
-//         properties: {
-//           sum: {
-//             [__render__]: [Info],
-//             data: $(node => {
-//               const { inputs } = node.$
-//               let sum = 0
-//               inputs.items.forEach(item => (sum += Number(item.data)))
-//               return sum
-//             }),
-//           },
-//           inputs: {
-//             [__render__]: [XArray],
-//             template: {
-//               [__render__]: [Input],
-//             },
-//             items: Array(1000)
-//               .fill(null)
-//               .map(() => ({
-//                 data: '0',
-//               })),
-//           },
+// return (
+//   <XForm
+//     schema={{
+//       [__render__]: [XObject],
+//       properties: {
+//         sum: {
+//           [__render__]: [Info],
+//           data: $(node => {
+//             const { inputs } = node.$
+//             let sum = 0
+//             inputs.items.forEach(item => (sum += Number(item.data)))
+//             return sum
+//           }),
 //         },
-//       }}
-//     />
-//   )
+//         inputs: {
+//           [__render__]: [XArray],
+//           template: {
+//             [__render__]: [Input],
+//           },
+//           items: Array(1000)
+//             .fill(null)
+//             .map(() => ({
+//               data: '0',
+//             })),
+//         },
+//       },
+//     }}
+//   />
+// )
 // }
 
 export default function TestCase() {
