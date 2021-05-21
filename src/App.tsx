254e7fae4ebb99fd44156173b645a1c38611a482
PerishCode <perishcode@gmail.com>
2021-05-21 15:02:16 +0800

    nothing

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,6 @@ import { Form, Editor, Viewer } from './components'
 import { initialData, initialSchema } from './schemas/select'
 import './App.less'
 
-fetch('/mock/schema/123', {
-  method: 'POST',
-})
-  .then(res => res.text())
-  .then(console.log)
-
 export default function App() {
   const [formData, setFormData] = useState(initialData)
   const [schema, setSchema] = useState(initialSchema)
